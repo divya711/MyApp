915974b1240ae59f8a1e753b895c6b45d2960ced
divya Singh <singhdivya711@gmail.com>
2024-01-16 22:46:53 +0530

    Added custom values properties

--- src/main/java/com/springboot/dev/MyApp/controller/ApplicationTest.java
+++ src/main/java/com/springboot/dev/MyApp/controller/ApplicationTest.java
@@ -1,13 +1,26 @@
 package com.springboot.dev.MyApp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationTest {
 
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${team.details}")
+    private String teamDescription;
+
+    @GetMapping("/teamInfo")
+    public String teamDetails(){
+        return teamName +" is team of " +teamDescription;
+    }
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";
     }
+
 }
